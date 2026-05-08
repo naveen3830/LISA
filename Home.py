768c9e873301953336d82e05a2bef8bb05a6b8ba
Maansi Tomer <maansitomer2002@gmail.com>
2026-05-08 14:39:31 +0530

    Implement custom exception class and error logging

--- Home.py
+++ Home.py
@@ -332,6 +332,30 @@ def Home():
                         full_response = error_message
                 
                 st.session_state.chat_history.append(AIMessage(content=full_response))
+                exception_code = """
+                import sys
+                
+                def error_message_detail(error,error_detail:sys):
+                    _,_,exc_tb=error_detail.exc_info()
+                    file_name=exc_tb.tb_frame.f_code.co_filename
+                    
+                    return "Error occurred in python script [{0}] line number [{1}] error message [{2}]".format(
+                        file_name,
+                        exc_tb.tb_lineno,
+                        str(error)
+                    )
+                
+                class CustomException(Exception):
+                    def __init__(self,error_message,error_detail:sys):
+                        super().__init__(error_message)
+                        self.error_message=error_message_detail(error_message,error_detail)
+                
+                    def __str__(self):
+                        return self.error_message
+                """
+                
+                with open(f"{project_name}/exception/__init__.py","w") as f:
+                    f.write(exception_code)
                 
     with tab3:
         st.header("LLM Model Card")
