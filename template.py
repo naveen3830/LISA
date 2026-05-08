3fb6029f35a9c46a0f2e82ae3edaceb72f3d597c
Maansi Tomer <maansitomer2002@gmail.com>
2026-05-08 12:32:28 +0530

    Implement logging functionality in template.py

--- template.py
+++ template.py
@@ -27,6 +27,28 @@ list_of_files=[
     "setup.py"
 ]
 
+logger_code = """
+import logging
+import os
+from datetime import datetime
+
+LOG_FILE=f"{datetime.now().strftime('%m_%d_%Y_%H_%M_%S')}.log"
+logs_path=os.path.join(os.getcwd(),"logs",LOG_FILE)
+
+os.makedirs(logs_path,exist_ok=True)
+
+LOG_FILE_PATH=os.path.join(logs_path,LOG_FILE)
+
+logging.basicConfig(
+    filename=LOG_FILE_PATH,
+    format="[ %(asctime)s ] %(lineno)d %(name)s - %(levelname)s - %(message)s",
+    level=logging.INFO,
+)
+"""
+
+with open(f"{project_name}/logger/__init__.py","w") as f:
+    f.write(logger_code)
+
 for file_path in list_of_files:
     filepath=Path(file_path)
     filedir,filename=os.path.split(filepath)
